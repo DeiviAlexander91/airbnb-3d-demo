321265927aff9af8bea38bf68d6ae96ab09cbb62
DeiviAlexander91 <daloland91@gmail.com>
2025-07-06 01:26:00 +0200

    Add files via upload

---
+++ components/Card.js
@@ -0,0 +1,12 @@
+// components/Card.js
+export function Card({ children }) {
+  return (
+    <div className="bg-white p-4 rounded shadow hover:shadow-md transition">
+      {children}
+    </div>
+  );
+}
+
+export function CardContent({ children }) {
+  return <div className="text-sm text-gray-700">{children}</div>;
+}
