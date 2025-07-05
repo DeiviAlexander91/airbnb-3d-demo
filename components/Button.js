e2903315adcfa00ddf7a152b6829190479e78116
DeiviAlexander91 <daloland91@gmail.com>
2025-07-06 01:21:37 +0200

    Delete components directory

--- components/Button.js
+++
@@ -1,8 +0,0 @@
-export default function Button({ children, asChild }) {
-  const Comp = asChild ? 'span' : 'button';
-  return (
-    <Comp className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
-      {children}
-    </Comp>
-  );
-}
