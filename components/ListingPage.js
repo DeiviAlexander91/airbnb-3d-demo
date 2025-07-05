810246b0358a985de7cf16abff36a5b2fa9db755
DeiviAlexander91 <daloland91@gmail.com>
2025-07-05 21:34:45 +0200

    Update ListingPage.js

--- components/ListingPage.js
+++ components/ListingPage.js
@@ -1,6 +1,6 @@
-import { Button } from "@/components/ui/button";
+import Button from "./Button";
 import Image from "next/image";
-import { Card, CardContent } from "@/components/ui/card";
+import Card from "./Cards";
 
 const highlights = [
   {
