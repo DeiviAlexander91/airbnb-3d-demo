3d3af3a06f5fc88e2c94330aa640ab18e897e4a4
DeiviAlexander91 <daloland91@gmail.com>
2025-07-07 13:35:03 +0200

    Add files via upload

---
+++ pages/_doukment.js
@@ -0,0 +1,20 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="no">
+      <Head>
+        {/* Font og ikoner */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body className="bg-[#fff9f6] text-black font-sans">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
