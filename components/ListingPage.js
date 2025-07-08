af7b4d9a57d6893f82bcca70b3c1ff9bd06e1a81
DeiviAlexander91 <daloland91@gmail.com>
2025-07-08 19:08:16 +0200

    Update ListingPage.js

--- components/ListingPage.js
+++ components/ListingPage.js
@@ -121,7 +121,6 @@
   target="_blank"
   rel="noopener noreferrer"
   class="inline-block px-6 py-3 bg-pink-500 text-white text-base rounded-full hover:bg-pink-600 transition"
->
   Start ferien din her
 </a>
 </div>
