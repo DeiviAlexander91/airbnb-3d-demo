79ab72a943a3661aa5cb89d4f3d538b4daa6c4cf
DeiviAlexander91 <daloland91@gmail.com>
2025-08-18 09:20:54 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'bounce-slow': 'bounce 3s infinite',
+      },
+    },
   },
   plugins: [],
 };
